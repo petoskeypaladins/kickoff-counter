f09638aa8098f043ea8a79ed07197db9b4f8aea2
Brendon-Egolf <begolf123@gmail.com>
2016-11-15 16:30:07 -0500

    Initial commit

---
+++ app/src/test/java/org/petoskeypaladins/petoskey_paladins_kickoff_timer/petoskeypaladinskickofftimer/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package org.petoskeypaladins.petoskey_paladins_kickoff_timer.petoskeypaladinskickofftimer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
